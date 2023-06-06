9f87f59a2a48caf7d063dcf064364a75d7c32f2b
Pedro-gd <e.mma.nu@gmail.com>
2023-06-05 23:11:01 -0600

    Proyecto Back

---
+++ EventickNow/src/main/java/com/venta/EventickNow/EventickNowApplication.java
@@ -0,0 +1,13 @@
+package com.venta.EventickNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventickNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventickNowApplication.class, args);
+	}
+
+}
