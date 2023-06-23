b26fa8e39643000797649739507234e39ac431c7
luislabradaa <87114168+luislabradaa@users.noreply.github.com>
2023-06-22 22:57:01 -0600

    Arreglos de validaciones min y max length

--- FrontendEventickNow/src/app/pages/registro-usuario/registro-usuario.component.ts
+++ FrontendEventickNow/src/app/pages/registro-usuario/registro-usuario.component.ts
@@ -3,18 +3,15 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BaseForm } from 'src/app/shared/utils/base-form';
 
-
 @Component({
   selector: 'app-registro-usuario',
   templateUrl: './registro-usuario.component.html',
   styleUrls: ['./registro-usuario.component.scss']
 })
 
-
 export class RegistroUsuarioComponent implements OnInit{
 
   registerForm: any;
-  
   
   //Crea las intancias a utilizar 
   constructor(
@@ -23,12 +20,14 @@ export class RegistroUsuarioComponent implements OnInit{
     private router:Router) {}
 
     ngOnInit(): void {
+
+      //Aplica las validaciones necesarias para el formulario de registro registerForm
       this.registerForm = this.fb.group({
-        name: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern(('^[A-Za-z]+$')),]],
-        lastname: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern(('^[A-Za-z]+$'))]],
+        name: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(20),Validators.pattern(('^[A-Za-z s]*$')),]],
+        lastname: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(20),Validators.pattern(('^[A-Za-z s]*$'))]],
         typeUser: ['',Validators.required],
         email: ['',[Validators.required,Validators.email]],
-        password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern(('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'))]],
+        password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(20)]],
 
       })
     }
