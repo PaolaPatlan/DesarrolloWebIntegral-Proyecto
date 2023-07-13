5e448b3273f43e606bc8e88a5ddcab32f572e4fd
luislabradaa <87114168+luislabradaa@users.noreply.github.com>
2023-07-13 02:25:34 -0600

    ConexiónRegistro

--- FrontendEventickNow/src/app/services/registro.service.ts
+++ FrontendEventickNow/src/app/services/registro.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+
+//Modelo para registrar usuario
+import { registerUser } from '../shared/models/nuevoUsuario';
+
+//Variable de conexión con backend
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistroService {
 
-  constructor() { }
+  //Conexión con el backend para insertar nuevo usuario
+  ///auth/nuevo;
+
+  constructor(private http: HttpClient) { }
+
+  
+  newUser(user: registerUser) {
+    return this.http.post(`${ environment.baseUrl }/auth/nuevo`, user)
+  }
 }
---
+++ FrontendEventickNow/src/app/shared/models/nuevoUsuario.ts
@@ -0,0 +1,11 @@
+export interface registerUser {
+
+    //id?: number;
+    nombre: string;
+    apellidoMaterno: string;
+    apellidoPaterno: string;
+    correoElectronico: string;
+    password: string;
+    roles: string;
+
+}
