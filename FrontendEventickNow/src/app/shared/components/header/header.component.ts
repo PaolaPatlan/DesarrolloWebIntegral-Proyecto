d70c92cf29fac070debf8d59b90ea352de39b952
luislabradaa <87114168+luislabradaa@users.noreply.github.com>
2023-08-10 22:18:56 -0600

    Componente usuario, API paypal, Generador de PDF y adecuaciones componente administrador y header

--- FrontendEventickNow/src/app/shared/components/header/header.component.ts
+++ FrontendEventickNow/src/app/shared/components/header/header.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { Subject, debounceTime, distinctUntilChanged } from 'rxjs';
-import { Evento } from '../../models/evento';
-import { UsuarioService } from 'src/app/services/usuario.service';
-
 
 @Component({
   selector: 'app-header',
@@ -11,63 +7,7 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class HeaderComponent {
 
-  private busquedaSubject = new Subject<string>();
-  public terminoBusqueda = '';
-  evento: Evento = { 
-    idEvento   :0,
-    nomEvento  :'',
-    fecha      :'',
-    ubicacion  :'',
-    detalles   :'',
-    estatus    :'',
-    costo      :'',
-    cantBoleto :'',
-    imagen     :'',
-  }; // Inicializar el objeto con la propiedad detalles
+  constructor() {}
 
-  resultadosBusqueda: any[] = [];
-
-  constructor(private eventoService: UsuarioService) {
-    // Suscripción para realizar la búsqueda después de un retraso de 500ms
-    this.busquedaSubject
-      .pipe(debounceTime(500), distinctUntilChanged())
-      .subscribe((terminoBusqueda: string) => {
-        this.realizarBusquedaEnServidor(terminoBusqueda); // Aquí puedes hacer la llamada a tu servidor para buscar eventos
-      });
-  }
-
-  realizarBusqueda() {
-    // Emitir el término de búsqueda para realizar la búsqueda después del retraso
-    this.busquedaSubject.next(this.terminoBusqueda);
-  }
-
-
-  realizarBusquedaEnServidor(terminoBusqueda: string) {
-    if (terminoBusqueda.trim() !== '') {
-      this.evento.detalles = terminoBusqueda;
-      // Implementa aquí la lógica para buscar eventos en tu servidor
-      this.eventoService.buscarEvento(this.evento).subscribe(data => {
-        this.resultadosBusqueda = data.list;
-        for (let index = 0; index < this.resultadosBusqueda.length; index++) {
-          const element = this.resultadosBusqueda[index];
-          this.getBase64Image(element.imagen).then((imageUrl) => {
-            element.imagen = imageUrl;
-          });
-        }
-        console.log(data.list);
-      });
-    } else {
-      this.resultadosBusqueda = [];
-    }
-  }
-
-  getBase64Image(base64Data: string): Promise<string> {
-    return new Promise<string>((resolve) => {
-      const imageUrl = `data:image/png;base64,${base64Data}`;
-      const img = new Image();
-      img.src = imageUrl;
-      img.onload = () => resolve(imageUrl);
-    });
-  }
 
 }
